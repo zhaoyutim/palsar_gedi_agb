6a22f5d5927d7293b13a0a7b59e1950eaee9e2a8
赵宇 <zhaoyu@zhaoyudeMBP5130.lan>
2023-03-05 20:03:01 +0100

    update transformer

--- run_abc_model.py
+++ run_abc_model.py
@@ -27,7 +27,7 @@ else:
 def wandb_config(model_name, num_layers, hidden_size):
     wandb.login()
     wandb.init(project="abc_challenge_"+model_name+"_grid_search", entity="zhaoyutim")
-    wandb.run.name = 'num_layers_'+ str(num_layers)+'hidden_size_'+str(hidden_size)+'batchsize_'+str(batch_size)
+    wandb.run.name = 'num_heads_' + str(num_heads) + 'num_layers_'+ str(num_layers)+ 'mlp_dim_'+str(mlp_dim)+'hidden_size_'+str(hidden_size)+'batchsize_'+str(batch_size)
     wandb.config = {
         "learning_rate": learning_rate,
         "weight_decay": weight_decay,
@@ -37,72 +37,99 @@ def wandb_config(model_name, num_layers, hidden_size):
         "embed_dim": hidden_size
     }
 
-def get_dataset():
+def get_dataset(channel_first=True):
     trainset = h5py.File("africa-biomass-challenge/09072022_1154_train.h5", "r")
     validateset = h5py.File("africa-biomass-challenge/09072022_1154_val.h5", "r")
     testset = h5py.File("africa-biomass-challenge/09072022_1154_test.h5", "r")
     def feature_engineering(array, scl, cloud, lat, lon):
-        dvi = array[:, [7], :, :] / (array[:, [3], :, :] + 1e6)
-        ndvi = (array[:, [7], :, :] - array[:, [3], :, :]) / (array[:, [7], :, :] + array[:, [3], :, :] + 1e6)
-        ndvi2 = (array[:, [8], :, :] - array[:, [3], :, :]) / (array[:, [8], :, :] + array[:, [3], :, :] + 1e6)
-        gndvi = (array[:, [7], :, :] - array[:, [2], :, :]) / (array[:, [7], :, :] + array[:, [2], :, :] + 1e6)
-        ndi45 = (array[:, [4], :, :] - array[:, [3], :, :]) / (array[:, [4], :, :] + array[:, [3], :, :] + 1e6)
-        ndre = (array[:, [7], :, :] - array[:, [4], :, :]) / (array[:, [7], :, :] + array[:, [4], :, :] + 1e6)
-        array = np.concatenate([array, scl, cloud, lat, lon, dvi, ndvi, ndvi2, gndvi, ndi45, ndre], axis=1)
+        dvi = array[:, :, :, [7]] / (array[:, :, :, [3]] + 1e6)
+        ndvi = (array[:, :, :, [7]] - array[:, :, :, [3]]) / (array[:, :, :, [7]] + array[:, :, :, [3]] + 1e6)
+        ndvi2 = (array[:, :, :, [8]] - array[:, :, :, [3]]) / (array[:, :, :, [8]] + array[:, :, :, [3]] + 1e6)
+        gndvi = (array[:, :, :, [7]] - array[:, :, :, [2]]) / (array[:, :, :, [7]] + array[:, :, :, [2]] + 1e6)
+        ndi45 = (array[:, :, :, [4]] - array[:, :, :, [3]]) / (array[:, :, :, [4]] + array[:, :, :, [3]] + 1e6)
+        ndre = (array[:, :, :, [7]] - array[:, :, :, [4]]) / (array[:, :, :, [7]] + array[:, :, :, [4]] + 1e6)
+        array = np.concatenate([array, scl, cloud, lat, lon, dvi, ndvi, ndvi2, gndvi, ndi45, ndre], axis=3)
+        if channel_first:
+            array = array.transpose(0, 3, 1, 2)
         return array
-
+    def patch_dividing(array):
+        patches = tf.image.extract_patches(
+            images=array,
+            sizes=[1, 5, 5, 1],
+            strides=[1, 5, 5, 1],
+            rates=[1, 1, 1, 1],
+            padding="VALID",
+        )
+        return patches.numpy().reshape(patches.shape[0], -1, patches.shape[3])
     # train
-    train_images = np.array(trainset['images'], dtype=np.float64).transpose(0, 3, 1, 2)
-    train_scl = np.array(trainset['scl'], dtype=np.float64).transpose(0, 3, 1, 2)
-    train_cloud = np.array(trainset['cloud'], dtype=np.float64).transpose(0, 3, 1, 2)
-    train_lat = np.array(trainset['lat'], dtype=np.float64).transpose(0, 3, 1, 2)
-    train_lon = np.array(trainset['lon'], dtype=np.float64).transpose(0, 3, 1, 2)
+    train_images = np.array(trainset['images'], dtype=np.float64)
+    train_scl = np.array(trainset['scl'], dtype=np.float64)
+    train_cloud = np.array(trainset['cloud'], dtype=np.float64)
+    train_lat = np.array(trainset['lat'], dtype=np.float64)
+    train_lon = np.array(trainset['lon'], dtype=np.float64)
     train_biomasses = np.array(trainset['agbd'], dtype=np.float64)
     train_images = feature_engineering(train_images, train_scl, train_cloud, train_lat, train_lon)
+    if not channel_first:
+        MEAN = train_images.mean((0, 1, 2))
+        STD = train_images.std((0, 1, 2))
+        train_images_norm = (train_images - MEAN[None, None, None, :]) / STD[None, None, None, :]
+        train_images_norm = patch_dividing(train_images_norm)
+    else:
+        MEAN = train_images.mean((0, 2, 3))
+        STD = train_images.std((0, 2, 3))
+        train_images_norm = (train_images - MEAN[None, :, None, None]) / STD[None, :, None, None]
+        train_images_norm = train_images_norm.reshape((train_images_norm.shape[0], train_images_norm.shape[1], -1))
 
     # validate
-    validate_images = np.array(validateset['images'], dtype=np.float64).transpose(0, 3, 1, 2)
-    validate_scl = np.array(validateset['scl'], dtype=np.float64).transpose(0, 3, 1, 2)
-    validate_cloud = np.array(validateset['cloud'], dtype=np.float64).transpose(0, 3, 1, 2)
-    validate_lat = np.array(validateset['lat'], dtype=np.float64).transpose(0, 3, 1, 2)
-    validate_lon = np.array(validateset['lon'], dtype=np.float64).transpose(0, 3, 1, 2)
+    validate_images = np.array(validateset['images'], dtype=np.float64)
+    validate_scl = np.array(validateset['scl'], dtype=np.float64)
+    validate_cloud = np.array(validateset['cloud'], dtype=np.float64)
+    validate_lat = np.array(validateset['lat'], dtype=np.float64)
+    validate_lon = np.array(validateset['lon'], dtype=np.float64)
     validate_biomasses = np.array(validateset['agbd'], dtype=np.float64)
     validate_images = feature_engineering(validate_images, validate_scl, validate_cloud, validate_lat, validate_lon)
+    if not channel_first:
+        validate_images_norm = (validate_images - MEAN[None, None, None, :]) / STD[None, None, None, :]
+        validate_images_norm = patch_dividing(validate_images_norm)
+    else:
+        validate_images_norm = (validate_images - MEAN[None, :, None, None]) / STD[None, :, None, None]
+        validate_images_norm = validate_images_norm.reshape(
+            (validate_images_norm.shape[0], validate_images_norm.shape[1], -1))
+
 
     # test
-    test_images = np.array(testset['images'], dtype=np.float32).transpose(0, 3, 1, 2)
-    test_scl = np.array(testset['scl'], dtype=np.float64).transpose(0, 3, 1, 2)
-    test_cloud = np.array(testset['cloud'], dtype=np.float64).transpose(0, 3, 1, 2)
-    test_lat = np.array(testset['lat'], dtype=np.float64).transpose(0, 3, 1, 2)
-    test_lon = np.array(testset['lon'], dtype=np.float64).transpose(0, 3, 1, 2)
+    test_images = np.array(testset['images'], dtype=np.float32)
+    test_scl = np.array(testset['scl'], dtype=np.float64)
+    test_cloud = np.array(testset['cloud'], dtype=np.float64)
+    test_lat = np.array(testset['lat'], dtype=np.float64)
+    test_lon = np.array(testset['lon'], dtype=np.float64)
     test_biomasses = np.array(testset['agbd'], dtype=np.float32)
     test_images = feature_engineering(test_images, test_scl, test_cloud, test_lat, test_lon)
+    if not channel_first:
+        test_images_norm = (test_images - MEAN[None, None, None, :]) / STD[None, None, None, :]
+        test_images_norm = patch_dividing(test_images_norm)
+    else:
+        test_images_norm = (test_images - MEAN[None, :, None, None]) / STD[None, :, None, None]
+        test_images_norm = test_images_norm.reshape((test_images_norm.shape[0], test_images_norm.shape[1], -1))
 
     # infer
     infer_images = h5py.File("africa-biomass-challenge/images_test.h5", "r")
-    infer_images = np.array(infer_images["images"]).transpose(0, 3, 1, 2)
+    infer_images = np.array(infer_images["images"])
     infer_scl = h5py.File("africa-biomass-challenge/scl_test.h5", "r")
-    infer_scl = np.array(infer_scl["scl"]).transpose(0, 3, 1, 2)
+    infer_scl = np.array(infer_scl["scl"])
     infer_cloud = h5py.File("africa-biomass-challenge/cloud_test.h5", "r")
-    infer_cloud = np.array(infer_cloud["cloud"]).transpose(0, 3, 1, 2)
+    infer_cloud = np.array(infer_cloud["cloud"])
     infer_lat = h5py.File("africa-biomass-challenge/lat_test.h5", "r")
-    infer_lat = np.array(infer_lat["lat"]).transpose(0, 3, 1, 2)
+    infer_lat = np.array(infer_lat["lat"])
     infer_lon = h5py.File("africa-biomass-challenge/lon_test.h5", "r")
-    infer_lon = np.array(infer_lon["lon"]).transpose(0, 3, 1, 2)
-
+    infer_lon = np.array(infer_lon["lon"])
     infer_images = feature_engineering(infer_images, infer_scl, infer_cloud, infer_lat, infer_lon)
-
-    MEAN = train_images.mean((0, 2, 3))
-    STD = train_images.std((0, 2, 3))
-    train_images_norm = (train_images - MEAN[None, :, None, None]) / STD[None, :, None, None]
-    validate_images_norm = (validate_images - MEAN[None, :, None, None]) / STD[None, :, None, None]
-    test_images_norm = (test_images - MEAN[None, :, None, None]) / STD[None, :, None, None]
-    infer_images_norm = (infer_images - MEAN[None, :, None, None]) / STD[None, :, None, None]
-
-    train_images_norm = train_images_norm.reshape((train_images_norm.shape[0], train_images_norm.shape[1], -1))
-    validate_images_norm = validate_images_norm.reshape((validate_images_norm.shape[0], validate_images_norm.shape[1], -1))
-    test_images_norm = test_images_norm.reshape((test_images_norm.shape[0], test_images_norm.shape[1], -1))
-    infer_images_norm = infer_images_norm.reshape((infer_images_norm.shape[0], infer_images_norm.shape[1], -1))
+    if not channel_first:
+        infer_images_norm = (infer_images - MEAN[None, None, None, :]) / STD[None, None, None, :]
+        infer_images_norm = patch_dividing(infer_images_norm)
+    else:
+        infer_images_norm = (infer_images - MEAN[None, :, None, None]) / STD[None, :, None, None]
+        infer_images_norm = infer_images_norm.reshape((infer_images_norm.shape[0], infer_images_norm.shape[1], -1))
 
     return train_images_norm, train_biomasses, validate_images_norm, validate_biomasses, test_images_norm, test_biomasses, infer_images_norm
 
@@ -130,7 +157,7 @@ if __name__=='__main__':
     weight_decay = lr/10
     MAX_EPOCHS = 50
 
-    train_images_norm, train_biomasses, validate_images_norm, validate_biomasses, test_images_norm, test_biomasses, infer_images_norm = get_dataset()
+    train_images_norm, train_biomasses, validate_images_norm, validate_biomasses, test_images_norm, test_biomasses, infer_images_norm = get_dataset(channel_first=False)
     input_shape = (train_images_norm.shape[1], train_images_norm.shape[2])
     wandb_config(model_name, num_layers=num_layers, hidden_size=hidden_size)
 
@@ -164,7 +191,7 @@ if __name__=='__main__':
             tf.keras.metrics.RootMeanSquaredError(name="rmse")
         ],
     )
-    checkpoint = ModelCheckpoint(os.path.join(root_path.replace('africa-biomass-challenge', 'abc_challenge_models'), 'abc_' + model_name + str(hidden_size) + '_' + str(num_layers)),
+    checkpoint = ModelCheckpoint(os.path.join(root_path.replace('africa-biomass-challenge', 'abc_challenge_models'), 'abc_' + 'num_heads_' + str(num_heads) + 'num_layers_'+ str(num_layers)+ 'mlp_dim_'+str(mlp_dim)+'hidden_size_'+str(hidden_size)+'batchsize_'+str(batch_size)),
                                  monitor="val_loss", mode="min", save_best_only=True, verbose=1)
     history = model.fit(
         x=train_images_norm,
@@ -175,7 +202,7 @@ if __name__=='__main__':
     )
     # model.save(os.path.join(root_path, 'proj3_' + model_name + str(hidden_size) + '_' + str(num_layers)))
 
-    model.load_weights(os.path.join(root_path.replace('africa-biomass-challenge', 'abc_challenge_models'), 'abc_' + model_name + str(hidden_size) + '_' + str(num_layers)))
+    model.load_weights(os.path.join(root_path.replace('africa-biomass-challenge', 'abc_challenge_models'), 'abc_' + 'num_heads_' + str(num_heads) + 'num_layers_'+ str(num_layers)+ 'mlp_dim_'+str(mlp_dim)+'hidden_size_'+str(hidden_size)+'batchsize_'+str(batch_size)))
 
     score = model.evaluate(test_images_norm, test_biomasses, verbose=0)
     print('Test loss:', score[0])
@@ -188,4 +215,4 @@ if __name__=='__main__':
     preds = ID_S2_pair.merge(preds, on='S2_idx').drop(columns=['S2_idx'])
     if not os.path.exists('africa-biomass-challenge/predictions'):
         os.mkdir('africa-biomass-challenge/predictions')
-    preds.to_csv('africa-biomass-challenge/predictions/biomass_predictions'+ model_name + str(hidden_size) + '_' + str(num_layers)+'.csv', index=False)
+    preds.to_csv('africa-biomass-challenge/predictions/biomass_predictions'+ model_name + 'num_heads_' + str(num_heads) + 'num_layers_'+ str(num_layers)+ 'mlp_dim_'+str(mlp_dim)+'hidden_size_'+str(hidden_size)+'batchsize_'+str(batch_size)+'.csv', index=False)
